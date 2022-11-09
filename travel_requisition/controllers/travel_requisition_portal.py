01545e6e819f1eaf410bfa0208e33ed0a8618544
amey204 <amey.planetodoo@gmail.com>
2022-11-09 17:06:26 +0530

    add chnages

--- travel_requisition/controllers/travel_requisition_portal.py
+++ travel_requisition/controllers/travel_requisition_portal.py
@@ -45,23 +45,23 @@ class AllExpense(http.Controller):
         cdate = date.today()
 
         # get default selected account based on search filter from many 2 one
-        t_account_id_def = request.env['account.account'].sudo().search(
-            [('internal_type', '=', 'other'), ('code', '=', '210700')])
+        # t_account_id_def = request.env['account.account'].sudo().search(
+        #     [('internal_type', '=', 'other'), ('code', '=', '210700')])
         # get all account list based on search filter from many 2 one
-        t_account_id = request.env['account.account'].sudo().search([('internal_type', '=', 'other')])
+        # t_account_id = request.env['account.account'].sudo().search([('internal_type', '=', 'other')])
 
         # get currency from many 2 one
-        t_currency_name = request.env['res.currency'].sudo().search([])
+        # t_currency_name = request.env['res.currency'].sudo().search([])
 
         # get all account list based on search filter from many 2 many
-        t_tax_name = request.env['account.tax'].sudo().search(
-            [('type_tax_use', '=', 'purchase'), ('price_include', '=', True)])
+        # t_tax_name = request.env['account.tax'].sudo().search(
+        #     [('type_tax_use', '=', 'purchase'), ('price_include', '=', True)])
 
         # get all analytic account list based on search filter from many 2 one
-        t_analytic_account = request.env['account.analytic.account'].sudo().search([])
+        # t_analytic_account = request.env['account.analytic.account'].sudo().search([])
 
         # get all account list based on search filter from many 2 many
-        t_analytic_account_tag = request.env['account.analytic.tag'].sudo().search([])
+        # t_analytic_account_tag = request.env['account.analytic.tag'].sudo().search([])
 
         # get field for getting paid by value
         # t_paid_id = request.env['hr.expense'].sudo().search([('travel_requisition_opt', '=', True)], limit=1)
@@ -71,22 +71,23 @@ class AllExpense(http.Controller):
         travel_detail_id_form = request.env['hr.expense'].sudo().search([])
         stay_detail_id_form = request.env['hr.expense'].sudo().search([])
 
-        # travel_detail_l = request.env['travel.details.line'].sudo().search([])
+        travel_detail_l = request.env['travel.details.line'].sudo().search([], limit=1)
         t_mode_class = request.env['mode.class.master'].sudo().search([])
         user_info = request.env['hr.employee'].sudo().browse(userid.id)
         # print(user_info.emp_code)
+        print(travel_detail_l.id)
 
         tautofill_data = {
             # 'module_fields_name' : defined_fields_name
             'user': userid,
             't_product': t_product,
             'cdate': cdate,
-            't_currency_name': t_currency_name,
-            't_tax_name': t_tax_name,
-            't_analytic_account': t_analytic_account,
-            't_analytic_account_tag': t_analytic_account_tag,
-            't_account_id': t_account_id,
-            't_account_id_def': t_account_id_def,
+            # 't_currency_name': t_currency_name,
+            # 't_tax_name': t_tax_name,
+            # 't_analytic_account': t_analytic_account,
+            # 't_analytic_account_tag': t_analytic_account_tag,
+            # 't_account_id': t_account_id,
+            # 't_account_id_def': t_account_id_def,
             # 't_paid_id': t_paid_id,
 
             't_empcode': user_info,
@@ -111,12 +112,12 @@ class AllExpense(http.Controller):
         if kw:
 
             # writing if condition to get value from many 2 one if value not present then pass false to the field
-            if kw.get('t_tax_name'):
-                # here tax_name many 2 many getting id in string is convert into integer and store in field
-                t_tax_ids = int(kw.get('t_tax_name'))
-            else:
-                # if tax_name not select then pass false
-                t_tax_ids = False
+            # if kw.get('t_tax_name'):
+            #     # here tax_name many 2 many getting id in string is convert into integer and store in field
+            #     t_tax_ids = int(kw.get('t_tax_name'))
+            # else:
+            #     # if tax_name not select then pass false
+            #     t_tax_ids = False
 
             if kw.get('t_product'):
                 # here r_product many 2 one getting id in string is convert into integer and store in field
@@ -125,22 +126,22 @@ class AllExpense(http.Controller):
                 # if product not select then pass false
                 t_product_name = False
 
-            if kw.get('t_account_id'):
-                t_accountid = int(kw.get('t_account_id'))
-            else:
-                t_accountid = False
+            # if kw.get('t_account_id'):
+            #     t_accountid = int(kw.get('t_account_id'))
+            # else:
+            #     t_accountid = False
 
-            if kw.get('t_analytic_account'):
-                t_analytic_acc = int(kw.get('t_analytic_account'))
-            else:
-                t_analytic_acc = False
+            # if kw.get('t_analytic_account'):
+            #     t_analytic_acc = int(kw.get('t_analytic_account'))
+            # else:
+            #     t_analytic_acc = False
 
-            if kw.get('t_analytic_account_tag'):
-                # here analytic_account_tag many 2 many getting id in string is convert into integer and store in field
-                t_analytic_tagid = int(kw.get('t_analytic_account_tag'))
-            else:
-                # if analytic_account_tag not select then pass false
-                t_analytic_tagid = False
+            # if kw.get('t_analytic_account_tag'):
+            #     # here analytic_account_tag many 2 many getting id in string is convert into integer and store in field
+            #     t_analytic_tagid = int(kw.get('t_analytic_account_tag'))
+            # else:
+            #     # if analytic_account_tag not select then pass false
+            #     t_analytic_tagid = False
 
             if kw.get('t_mode_class'):
                 # here t_mode_class many 2 one getting id in string is convert into integer and store in field
@@ -151,16 +152,17 @@ class AllExpense(http.Controller):
             tvals = {
                 'name': kw.get('t_expname'),
                 'product_id': t_product_name,
+                'total_amount': kw.get('t_total_amount'),
 
                 # this if condition and security condition is for many 2 many field
-                'tax_ids': False if t_tax_ids is False else [(4, t_tax_ids)],
+                # 'tax_ids': False if t_tax_ids is False else [(4, t_tax_ids)],
 
-                'reference': kw.get('ref'),
-                'account_id': t_accountid,
-                'analytic_account_id': t_analytic_acc,
+                # 'reference': kw.get('ref'),
+                # 'account_id': t_accountid,
+                # 'analytic_account_id': t_analytic_acc,
 
                 # this if condition and security condition is for many 2 many field
-                'analytic_tag_ids': False if t_analytic_tagid is False else [(4, t_analytic_tagid)],
+                # 'analytic_tag_ids': False if t_analytic_tagid is False else [(4, t_analytic_tagid)],
 
                 # don't know about this field, this field shows mandatory therefore i pass 1
                 'unit_amount': 1,
@@ -170,12 +172,12 @@ class AllExpense(http.Controller):
 
                 'purpose_of_visit': kw.get('t_purpose'),
 
-                'travel_detail_l.date': kw.get('t_date'),
-                'travel_detail_l.from_dates': kw.get('t_from'),
-                'travel_detail_l.departs_time': kw.get('t_depart_time'),
-                'travel_detail_l.to_dates': kw.get('t_to'),
-                'travel_detail_l.arrives_time': kw.get('t_arrive_time'),
-                'travel_detail_l.mode_and_class': t_modeclass,
+                'date': kw.get('t_date'),
+                'from_dates': kw.get('t_from'),
+                'departs_time': kw.get('t_depart_time'),
+                'to_dates': kw.get('t_to'),
+                'arrives_time': kw.get('t_arrive_time'),
+                'mode_and_class': t_modeclass,
             }
 
             # create method override to create record from form
--- travel_requisition/static/src/js/portal.js
+++ travel_requisition/static/src/js/portal.js
@@ -1,13 +1,120 @@
-//this is for drop menu of checkout page
-//$(document).on('change', '#r_product', function() {
-//    var product = $(this).val();
-//    console.log(product)
+//odoo.define('stack_overflow.open_list_view', function (require) {
 //
+//    "use strict";
+//    var pyUtils = require('web.py_utils');
+//    var dialogs = require('web.view_dialogs');
+//    var core = require('web.core');
+//    var _t = core._t;
+//    var fieldRegistry = require('web.field_registry');
+//    var ListRenderer = require('web.ListRenderer');
+//
+//    var AddManyFieldOne2ManyRenderer = ListRenderer.extend({
+//        /**
+//         * It will returns the first visible widget that is editable
+//         *
+//         * @private
+//         * @returns {Class} Widget returns first widget
+//         */
+//        _getFirstWidget: function () {
+//            var record = this.state.data[this.currentRow];
+//            var recordWidgets = this.allFieldWidgets[record.id];
+//            var firstWidget = _.chain(recordWidgets).filter(function (widget) {
+//                var isLast =
+//                    widget.$el.is(':visible') &&
+//                    (
+//                        widget.$('input').length > 0 || widget.tagName === 'input' ||
+//                        widget.$('textarea').length > 0 || widget.tagName === 'textarea'
+//                    ) &&
+//                    !widget.$el.hasClass('o_readonly_modifier');
+//                return isLast;
+//            }).first().value();
+//            return firstWidget;
+//        },
+//
+//        add_rows: function (lines, field_name) {
+//            var self = this;
+//            if (lines.length > 0) {
+//                self.trigger_up('add_record', {
+//                    context: [{ [field_name]: lines[0] }],
+//                    forceEditable: "bottom",
+//                    allowWarning: true,
+//                    onSuccess: function () {
+//                        self.unselectRow();
+//                        lines.shift(); // Remove the first element after adding a line
+//                        self.add_rows(lines, field_name);
+//                    }
+//                });
+//
+//            }
+//        },
+//        _onAddRecord: function (ev) {
+//            // we don't want the browser to navigate to a the # url
+//            ev.preventDefault();
+//
+//            // we don't want the click to cause other effects, such as unselecting
+//            // the row that we are creating, because it counts as a click on a tr
+//            ev.stopPropagation();
+//
+//
+//            var self = this;
+//            // but we do want to unselect current row
+//            this.unselectRow().then(function () {
+//                var context = ev.currentTarget.dataset.context;
+//                if (context && pyUtils.py_eval(context).open_list_view) {
+//                    // trigger add_record to get field name and model
+//                    // you do not need to trigger add_record if you pass the field name and model in context
+//                    self.trigger_up('add_record', {
+//                        context: [{}],
+//                        onSuccess: function () {
+//                            var widget = self._getFirstWidget();
+//                            var field_name = 'default_' + widget.name;
+//                            var model = widget.field.relation;
+//                            self.unselectRow();
+//                            self._rpc({
+//                                model: model,
+//                                method: 'search',
+//                                args: [[]],
+//                            }).then(
+//                                function (result) {
+//                                    return new dialogs.SelectCreateDialog(self, _.extend({}, self.nodeOptions, {
+//                                        res_model: model,
+//                                        context: context,
+//                                        title: _t("Search: add lines"),
+//                                        initial_ids: result,
+//                                        initial_view: 'search',
+//                                        disable_multiple_selection: false,
+//                                        no_create: !self.can_create,
+//                                        on_selected: function (records) {
+//                                            self.add_rows(records.map(product => product.id), field_name);
+//                                        }
+//                                    })).open();
+//                                });
+//                        }
+//                    });
+//                } else {
+//                    self.trigger_up('add_record', { context: context && [context] });
+//                }
+//            });
+//        },
+//    });
+//
+//    var AddManyFieldOne2Many = fieldRegistry.map.section_and_note_one2many.extend({
+//        /**
+//         * We want to use our custom renderer for the list.
+//         *
+//         * @override
+//         */
+//        _getRenderer: function () {
+//            if (this.view.arch.tag === 'tree') {
+//                return AddManyFieldOne2ManyRenderer;
+//            }
+//            return this._super.apply(this, arguments);
+//        },
+//    });
+//
+//    fieldRegistry.add('add_many_one2many', AddManyFieldOne2Many);
 //});
 
-//var base = document.getElementById("r_product").onchange = function() {myFunction()};
-//console.log(base)
-//function myFunction() {
-//  var product = $(this).val();
-//  console.log(product)
-//}
+function addRow(){
+alert('adding row')
+}
